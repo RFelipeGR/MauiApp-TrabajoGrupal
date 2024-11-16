a26568796552f725cf0ec06cce8435cedee560c7
roberto.guaña <robertofelipe20.rf@gmail.com>
2024-11-16 14:59:47 -0500

    PAGINA 2 - TERMINADO

--- MauiApp1/Page2.xaml.cs
+++ MauiApp1/Page2.xaml.cs
@@ -1,10 +1,17 @@
 
+
 namespace MauiApp1;
 
 public partial class Page2 : ContentPage
 {
+    
 	public Page2()
 	{
 		InitializeComponent();
 	}
+
+    private async void BotonPage2_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Page3());
+    }
 }
