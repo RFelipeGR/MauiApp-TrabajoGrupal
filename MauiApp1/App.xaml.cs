507717c52ece8b5a055675162ece555cfe9b8dd8
roberto.guaña <robertofelipe20.rf@gmail.com>
2024-11-16 13:21:32 -0500

    Agregar archivos de proyecto.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿/*namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}*/
+using MauiApp1;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // Usamos NavigationPage para que se pueda navegar entre páginas
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void BotonPrueba_Clicked(object sender, EventArgs e)
+        {
+            count=count+3;
+            Label1.Text = "La suma es " + count;
+            
+        }
+
+        private async void BotonNavegacion_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewPage1());
+        }
+    }
+
+}
